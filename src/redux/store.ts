b7062871a15935649b0c180a5b1bb1a6a3faf618
Mehedi Hasan <mehedi@etlimited.net>
2024-12-02 18:21:15 +0600

    28-4

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./featues/auth/authSlice";
 
 export const reduxStore = configureStore({
- reducer: {},
+ reducer: {
+  auth: authReducer
+ },
 })
 
 
